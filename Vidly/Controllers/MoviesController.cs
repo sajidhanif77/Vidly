d9ccdb55fbdd2ea32e6b07ae1e7026001718d3ca
shanif <shanif@LPT-SHANIF>
2017-08-22 08:42:20 +0200

    parameters exercise done

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: /Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+            return View(movie);
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id =" +id);
+        }
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}& sortby={1}", pageIndex,sortBy));
+        }
+    }
+}
